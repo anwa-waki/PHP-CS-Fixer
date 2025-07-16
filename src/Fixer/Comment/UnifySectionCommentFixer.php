381b4eb86246edcfb4aec68e0565c6aa57b0a5bd
Wasner <andreas.wasner@waki-software.de>
2025-07-16 16:11:25 +0200

    Clean implementation

--- src/Fixer/Comment/UnifySectionCommentFixer.php
+++ src/Fixer/Comment/UnifySectionCommentFixer.php
@@ -5,14 +5,43 @@ declare(strict_types=1);
 namespace PhpCsFixer\Fixer\Comment;
 
 use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\Fixer\ConfigurableFixerInterface;
+use PhpCsFixer\Fixer\ConfigurableFixerTrait;
+use PhpCsFixer\FixerConfiguration\FixerConfigurationResolver;
+use PhpCsFixer\FixerConfiguration\FixerConfigurationResolverInterface;
+use PhpCsFixer\FixerConfiguration\FixerOptionBuilder;
 use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
-use PhpCsFixer\Preg;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 
-final class UnifySectionCommentFixer extends AbstractFixer
+/**
+ * @psalm-suppress InternalClass
+ *
+ * @psalm-type InputConfig array{
+ *  section_names?: list<string>,
+ * }
+ * @psalm-type ComputedConfig array{
+ *  section_names: list<string>,
+ * }
+ *
+ * @template-implements ConfigurableFixerInterface<InputConfig, ComputedConfig>
+ */
+final class UnifySectionCommentFixer extends AbstractFixer implements ConfigurableFixerInterface
 {
+    /** @use ConfigurableFixerTrait<InputConfig, ComputedConfig> */
+    use ConfigurableFixerTrait;
+
+    /** @var list<string> */
+    private array $sectionNamesToFix;
+
+    #[\Override]
+    public function getName(): string
+    {
+        return 'UnifySectionCommentFixer/unify_section_comments';
+    }
+
+    #[\Override]
     public function getDefinition(): FixerDefinition
     {
         return new FixerDefinition(
@@ -20,27 +49,55 @@ final class UnifySectionCommentFixer extends AbstractFixer
             [
                 new CodeSample(
                     "<?php
-// Arrange\n"
+// Section\n",
                 ),
-            ]
+                new CodeSample(
+                    "<?php
+// Arrange\n",
+                    ['section_names' => ['Arrange']],
+                ),
+                new CodeSample(
+                    "<?php
+// Act & Assert\n",
+                    ['section_names' => ['Arrange', 'Act & Assert']],
+                ),
+            ],
         );
     }
 
+    #[\Override]
+    public function getPriority(): int
+    {
+        // Should be run extremely late, after all other comment fixers
+        return -999;
+    }
+
+    #[\Override]
     public function isCandidate(Tokens $tokens): bool
     {
         return $tokens->isTokenKindFound(T_COMMENT);
     }
 
+    protected function configurePostNormalisation(): void
+    {
+        $this->sectionNamesToFix = $this->configuration['section_names'] ?? [];
+    }
+
+    #[\Override]
     protected function applyFix(\SplFileInfo $file, Tokens $tokens): void
     {
-        for ($index = \count($tokens) - 1; 0 <= $index; --$index) {
-            $token = $tokens[$index];
+        /**
+         * @psalm-var int $index
+         * @psalm-var Token $token
+         */
+        foreach ($tokens as $index => $token) {
             if (!$token->isGivenKind(T_COMMENT)) {
                 continue;
             }
 
             $content = $token->getContent();
-            if ($this->doesContentAlreadyMatchSectionCommentFormat($content)) {
+
+            if (!$this->isCommentAFixableSectionName($content)) {
                 continue;
             }
 
@@ -49,15 +106,30 @@ final class UnifySectionCommentFixer extends AbstractFixer
         }
     }
 
-    private function doesContentAlreadyMatchSectionCommentFormat(string $content): bool
+    private function isCommentAFixableSectionName(string $content): bool
     {
-        return Preg::match('/^\/\/\s--\s.+/', $content);
+        $commentPart = mb_substr($content, 0, 3);
+        $sectionName = mb_substr($content, 3); // The comment without the leading '// '
+
+        return $commentPart === '// '
+               && in_array($sectionName, $this->sectionNamesToFix, true);
     }
 
     private function insertDashes(string $content): string
     {
-        $sectionName = substr($content, 3); // The comment without the leading '// '
+        $sectionName = mb_substr($content, 3); // The comment without the leading '// '
 
         return sprintf('// -- %s', $sectionName);
+    }
+
+    #[\Override]
+    protected function createConfigurationDefinition(): FixerConfigurationResolverInterface
+    {
+        return new FixerConfigurationResolver([
+            (new FixerOptionBuilder('section_names', 'List of section names to fix. i.e. Arrange, Act, Validate etc.'))
+                ->setAllowedTypes(['string[]'])
+                ->setDefault(['Section']) // PHP-CS-Fixer requires you to have a default to pass the tests. And that default must be the first CodeSample and that CodeSample must lead to a fix... So I have to use some nonsense default value to satisfy that requirement.
+                ->getOption(),
+        ]);
     }
 }
--- tests/Fixer/Comment/UnifySectionCommentFixerTest.php
+++ tests/Fixer/Comment/UnifySectionCommentFixerTest.php
@@ -4,21 +4,36 @@ declare(strict_types=1);
 
 namespace PhpCsFixer\Tests\Fixer\Comment;
 
+use PhpCsFixer\ConfigurationException\InvalidFixerConfigurationException;
 use PhpCsFixer\Tests\Test\AbstractFixerTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 
 final class UnifySectionCommentFixerTest extends AbstractFixerTestCase
 {
-    #[DataProvider('provideFixCases')]
-    public function testFix(string $expected, ?string $input = null): void
+    public function testInvalidConfiguration(): void
     {
+        $this->expectException(InvalidFixerConfigurationException::class);
+
+        $this->fixer->configure(['abc']);
+    }
+
+    #[DataProvider('provideFixCases')]
+    public function testFix(string $expected, ?string $input = null, array $configuration = []): void
+    {
+        $this->fixer->configure($configuration);
         $this->doTest($expected, $input);
     }
 
     /**
+     * The first value is the expected output.
+     * The second value is the input. If it is null, the fixer will expect it to be equal to the expected output.
+     * So this is for testing that nothing was changed.
+     * The third value is the configuration for the fixer.
+     *
      * @return array<array{
      *   0: string,
-     *   1?: ?string
+     *   1?: ?string,
+     *   2: array<string, array<string, string>>
      * }>
      */
     public static function provideFixCases(): array
@@ -29,6 +44,32 @@ final class UnifySectionCommentFixerTest extends AbstractFixerTestCase
 // -- Arrange',
                 '<?php
 // Arrange',
+                ['section_names' => ['Arrange']],
+            ],
+            [
+                '<?php
+// -- Act & Assert',
+                '<?php
+// Act & Assert',
+                ['section_names' => ['Arrange', 'Act & Assert']],
+            ],
+            [
+                '<?php
+// -- Arrange',
+                null,
+                ['section_names' => ['Arrange']],
+            ],
+            [
+                '<?php
+// Hurz',
+                null,
+                ['section_names' => ['Arrange']],
+            ],
+            [
+                '<?php
+/* Arrange */',
+                null,
+                ['section_names' => ['Arrange']],
             ],
         ];
     }
